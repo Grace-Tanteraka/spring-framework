70f6f3de3383eacd6d099dd987545b800485b3e0
MAMINIAVO Grace Tanteraka <gracetanteraka@gmail.com>
2026-06-11 09:22:45 +0300

    feat: implement RequestHandler servlet with doGet and doPost methods

---
+++ com/graceframework/controllers/RequestHandler.java
@@ -0,0 +1,26 @@
+package com.graceframework.controllers;
+
+import java.io.IOException;
+
+import jakarta.servlet.*;
+import jakarta.servlet.http.*;
+
+public class RequestHandler extends HttpServlet {
+    protected void doGet(
+            HttpServletRequest req,
+            HttpServletResponse resp) throws ServletException, IOException {
+
+    }
+
+    protected void doPost(
+            HttpServletRequest req,
+            HttpServletResponse resp) throws ServletException, IOException {
+
+    }
+
+    protected void handleRequest(
+            HttpServletRequest req,
+            HttpServletResponse resp) throws ServletException, IOException {
+        
+    }
+}
