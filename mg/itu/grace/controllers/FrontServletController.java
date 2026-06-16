6d919af713db19bdebe55842a6bdc5923cb582f3
MAMINIAVO Grace Tanteraka <gracetanteraka@gmail.com>
2026-06-16 13:54:12 +0300

    refactor : reoganise files and the package and rename the RequestHandler to FrontServletController

--- mg/itu/grace/controllers/FrontServletController.java
+++ mg/itu/grace/controllers/FrontServletController.java
@@ -1,4 +1,4 @@
-package com.graceframework.controllers;
+package mg.itu.grace.controllers;
 
 import java.io.IOException;
 import java.io.PrintWriter;
@@ -6,7 +6,7 @@ import java.io.PrintWriter;
 import jakarta.servlet.*;
 import jakarta.servlet.http.*;
 
-public class RequestHandler extends HttpServlet {
+public class FrontServletController extends HttpServlet {
     protected void doGet(
             HttpServletRequest req,
             HttpServletResponse resp) throws ServletException, IOException {
@@ -22,6 +22,17 @@ public class RequestHandler extends HttpServlet {
     protected void handleRequest(
             HttpServletRequest req,
             HttpServletResponse resp) throws ServletException, IOException {
+
+        String url = req.getRequestURL().toString();
+
+        String[] endPathUsingDefault = {".html", ".css", ".js", ".png", ".jpg", ".gif", ".ico", ".woff", ".woff2", ".ttf", ".eot", ".svg", ".mp4", ".webm", ".ogg", ".mp3", ".wav", ".pdf", ".jsp", ".json", ".xml", ".txt", ".csv", ".zip", ".tar", ".gz", ".rar", ".7z"};
+        for (String end : endPathUsingDefault) {
+            if (url.endsWith(end)) {
+                req.getServletContext().getNamedDispatcher("default").forward(req, resp);
+                return;
+            }
+        }
+
         PrintWriter out = resp.getWriter();
         out.println("Welcome to the Request Handler!");
     }
