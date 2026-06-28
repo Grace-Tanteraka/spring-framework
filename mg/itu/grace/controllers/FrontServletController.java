ecca7a23fcbd9171d70bcf4cda38045de9ceff93
MAMINIAVO Grace Tanteraka <gracetanteraka@gmail.com>
2026-06-28 17:06:58 +0300

    refactor: update FrontServletController to use UrlMethod for URL and HTTP method handling

--- mg/itu/grace/controllers/FrontServletController.java
+++ mg/itu/grace/controllers/FrontServletController.java
@@ -9,6 +9,7 @@ import jakarta.servlet.http.*;
 import mg.itu.grace.utils.ClassScanner;
 import mg.itu.grace.annotations.Controller;
 import mg.itu.grace.dto.ControllerMethod;
+import mg.itu.grace.dto.UrlMethod;
 
 import java.util.List;
 import java.util.Map;
@@ -16,7 +17,7 @@ import java.util.HashMap;
 public class FrontServletController extends HttpServlet {
     private ClassScanner classScanner;
     private List<Class<?>> controllerClasses;
-    private Map<String, ControllerMethod> urlMethodMap = new HashMap<>();
+    private Map<UrlMethod, ControllerMethod> urlMethodMap = new HashMap<>();
 
     public void init() throws ServletException {
         classScanner = new ClassScanner();
@@ -64,17 +65,18 @@ public class FrontServletController extends HttpServlet {
         url = classScanner.formatUrl(req);
         ControllerMethod match = null;
         try {
-            match = classScanner.isSupportedUrl(url, urlMethodMap); 
-            String toPrint = url + " -> " + match.getControllerClass().getName() + " (" + match.getAssociatedMethod().getName() + ")"; 
+            UrlMethod urlMethod = new UrlMethod(url, req.getMethod());
+            match = classScanner.validateUrlMethod(urlMethod, urlMethodMap);
+            String toPrint = urlMethod.toString() + " -> " + match.getControllerClass().getName() + " (" + match.getAssociatedMethod().getName() + ")"; 
             out.println(toPrint);
         } catch (Exception e) {
             out.println(e.getMessage() + "\n");
             out.println("Supported URLs:");
 
             // Print all supported URLs
-            for (String supportedUrl : urlMethodMap.keySet()) {
+            for (UrlMethod supportedUrl : urlMethodMap.keySet()) {
                 ControllerMethod method = urlMethodMap.get(supportedUrl);
-                String toPrint = supportedUrl + " -> " + method.getControllerClass().getName() + " (" + method.getAssociatedMethod().getName() + ")";
+                String toPrint = supportedUrl.toString() + " -> " + method.getControllerClass().getName() + " (" + method.getAssociatedMethod().getName() + ")";
                 out.println(toPrint);
             }
         }
