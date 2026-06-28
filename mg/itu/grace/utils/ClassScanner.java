3a8acc6f863302082d5f4192a1d53b9b2ce3c473
MAMINIAVO Grace Tanteraka <gracetanteraka@gmail.com>
2026-06-28 16:11:33 +0300

    refactor: update ClassScanner to use ControllerMethod and improve URL handling

--- mg/itu/grace/utils/ClassScanner.java
+++ mg/itu/grace/utils/ClassScanner.java
@@ -4,49 +4,86 @@ import java.util.List;
 import java.io.File;
 import java.net.URL;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 import java.lang.annotation.Annotation;
+import java.lang.reflect.Method;
 
 import jakarta.servlet.http.*;
-
-import mg.itu.grace.dto.ControllerMethodUrlDto;
+import mg.itu.grace.dto.ControllerMethod;
+import mg.itu.grace.annotations.*;
 
 public class ClassScanner {
-    public static List<ControllerMethodUrlDto> findSupportedUrl(String controllerPackage) {
-        List<Class<?>> controllerClasses = findAnnotatedClasses(mg.itu.grace.annotations.Controller.class,
-                controllerPackage);
-        List<ControllerMethodUrlDto> supportedUrls = new ArrayList<>();
-        for (Class<?> clazz : controllerClasses) {
-            if (clazz.isAnnotationPresent(mg.itu.grace.annotations.Controller.class)) {
-                for (java.lang.reflect.Method method : clazz.getDeclaredMethods()) {
-                    if (method.isAnnotationPresent(mg.itu.grace.annotations.UrlMapping.class)) {
-                        mg.itu.grace.annotations.UrlMapping urlMapping = method
-                                .getAnnotation(mg.itu.grace.annotations.UrlMapping.class);
+    public String formatUrl(HttpServletRequest req) {
+        String baseUrl = req.getRequestURI();
+        String contextPath = req.getContextPath();
+        return baseUrl.substring(contextPath.length());
+    }
 
-                        supportedUrls
-                                .add(new ControllerMethodUrlDto(urlMapping.url(), clazz.getName(), method.getName()));
+    public ControllerMethod isSupportedUrl(String url, Map<String, ControllerMethod> supportedUrls) throws Exception {
+        if (!supportedUrls.containsKey(url))
+            throw new Exception("No matching URL found for: " + url);
+        return supportedUrls.get(url);
+    }
+
+    public List<Class<?>> findControllerClasses(String packageName, Map<String, ControllerMethod> methodUrlsmap) {
+        List<Class<?>> list = new ArrayList<>();
+
+        if (packageName == null || packageName.isEmpty() || packageName.equalsIgnoreCase("ALL")) {
+            packageName = "";
+        }
+
+        try {
+            String path = packageName.replace('.', '/');
+            ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
+
+            URL resource = classLoader.getResource(path);
+
+            if (resource != null) {
+                File directory = new File(resource.getFile());
+                if (directory.exists()) {
+                    scanDirectoryForController(directory, packageName, list, methodUrlsmap);
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return list;
+    }
+
+    public void scanDirectoryForController(File directory, String packageName, List<Class<?>> classes,
+            Map<String, ControllerMethod> methodUrlsmap)
+            throws ClassNotFoundException {
+        File[] files = directory.listFiles();
+        if (files == null)
+            return;
+
+        for (File file : files) {
+            if (file.isDirectory()) {
+                String subPackage = packageName.isEmpty() ? file.getName() : packageName + "." + file.getName();
+                scanDirectory(file, subPackage, classes);
+            } else if (file.getName().endsWith(".class")) {
+                String className = file.getName().substring(0, file.getName().length() - 6);
+                String fullClassName = packageName.isEmpty() ? className : packageName + "." + className;
+
+                Class<?> clazz = Class.forName(fullClassName);
+
+                if (clazz.isAnnotationPresent(Controller.class)) {
+                    classes.add(clazz);
+                    Method[] belongedMethods = clazz.getDeclaredMethods();
+                    for (Method meth : belongedMethods) {
+                        if (meth.isAnnotationPresent(UrlMapping.class)) {
+                            UrlMapping urlMapping = meth.getAnnotation(UrlMapping.class);
+                            methodUrlsmap.put(urlMapping.url(), new ControllerMethod(clazz, meth));
+                        }
                     }
                 }
             }
         }
-        return supportedUrls;
     }
 
-    public static String formatUrl(HttpServletRequest req) {
-        String baseUrl = req.getRequestURI();
-        String contextPath = req.getContextPath();
-        return  baseUrl.substring(contextPath.length());
-    }
-
-    public static ControllerMethodUrlDto isSupportedUrl(String url, List<ControllerMethodUrlDto> supportedUrls) throws Exception{
-        for (ControllerMethodUrlDto match : supportedUrls) {
-            if(url.equals(match.getUrl()) || url.equals(match.getUrl()+"/")) {
-                return match;
-            }
-        }
-        throw new Exception("No matching URL found for: " + url);
-    }
-
-    public static List<String> findAnnotatedClassNames(Class<? extends Annotation> annotationClass,
+    public List<String> findAnnotatedClassNames(Class<? extends Annotation> annotationClass,
             String packageName) {
         List<String> controllerClassNames = new ArrayList<>();
         List<Class<?>> classes = findAnnotatedClasses(annotationClass, packageName);
@@ -57,7 +94,7 @@ public class ClassScanner {
         return controllerClassNames;
     }
 
-    public static List<Class<?>> findAnnotatedClasses(Class<? extends Annotation> annotationClass, String packageName) {
+    public List<Class<?>> findAnnotatedClasses(Class<? extends Annotation> annotationClass, String packageName) {
         List<Class<?>> controllerClasses = new ArrayList<>();
         List<Class<?>> classes = findClassInPackage(packageName);
         for (Class<?> clazz : classes) {
@@ -69,7 +106,7 @@ public class ClassScanner {
         return controllerClasses;
     }
 
-    private static List<Class<?>> findClassInPackage(String packageName) {
+    private List<Class<?>> findClassInPackage(String packageName) {
         List<Class<?>> classes = new ArrayList<>();
 
         if (packageName == null || packageName.isEmpty() || packageName.equalsIgnoreCase("ALL")) {
@@ -95,7 +132,8 @@ public class ClassScanner {
         return classes;
     }
 
-    private static void scanDirectory(File directory, String packageName, List<Class<?>> classes)
+
+    private void scanDirectory(File directory, String packageName, List<Class<?>> classes)
             throws ClassNotFoundException {
         File[] files = directory.listFiles();
         if (files == null)
