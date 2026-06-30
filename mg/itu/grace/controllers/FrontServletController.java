c5447383b6922e6fb84cbe2e6e2eb15dc9db1aca
MAMINIAVO Grace Tanteraka <gracetanteraka@gmail.com>
2026-06-30 10:56:29 +0300

    refactor: handle url duclication Exception

--- mg/itu/grace/controllers/FrontServletController.java
+++ mg/itu/grace/controllers/FrontServletController.java
@@ -14,6 +14,7 @@ import mg.itu.grace.dto.UrlMethod;
 import java.util.List;
 import java.util.Map;
 import java.util.HashMap;
+
 public class FrontServletController extends HttpServlet {
     private ClassScanner classScanner;
     private List<Class<?>> controllerClasses;
@@ -22,13 +23,18 @@ public class FrontServletController extends HttpServlet {
     public void init() throws ServletException {
         classScanner = new ClassScanner();
         String longPackageName = getInitParameter("controller-base-package");
-        if (longPackageName == null || longPackageName.isEmpty()) {
-            controllerClasses = classScanner.findControllerClasses("ALL", urlMethodMap);
-        } else {
-            String[] packageName = longPackageName.split(";");
-            for (String pkg : packageName) {
-                controllerClasses = classScanner.findControllerClasses(pkg, urlMethodMap);
+        controllerClasses = new java.util.ArrayList<>();
+        try {
+            if (longPackageName == null || longPackageName.isEmpty()) {
+                controllerClasses.addAll(classScanner.findControllerClasses("ALL", urlMethodMap));
+            } else {
+                String[] packageNames = longPackageName.split(";");
+                for (String pkg : packageNames) {
+                    controllerClasses.addAll(classScanner.findControllerClasses(pkg.trim(), urlMethodMap));
+                }
             }
+        } catch (Exception e) {
+            throw new ServletException("Échec de l'initialisation du FrontServletController : " + e.getMessage(), e);
         }
     }
 
@@ -49,9 +55,10 @@ public class FrontServletController extends HttpServlet {
             HttpServletResponse resp) throws ServletException, IOException {
 
         String url = req.getRequestURL().toString();
-        
 
-        String[] endPathUsingDefault = {".html", ".css", ".js", ".png", ".jpg", ".gif", ".ico", ".woff", ".woff2", ".ttf", ".eot", ".svg", ".mp4", ".webm", ".ogg", ".mp3", ".wav", ".pdf", ".jsp", ".json", ".xml", ".txt", ".csv", ".zip", ".tar", ".gz", ".rar", ".7z"};
+        String[] endPathUsingDefault = { ".html", ".css", ".js", ".png", ".jpg", ".gif", ".ico", ".woff", ".woff2",
+                ".ttf", ".eot", ".svg", ".mp4", ".webm", ".ogg", ".mp3", ".wav", ".pdf", ".jsp", ".json", ".xml",
+                ".txt", ".csv", ".zip", ".tar", ".gz", ".rar", ".7z" };
         for (String end : endPathUsingDefault) {
             if (url.endsWith(end)) {
                 req.getServletContext().getNamedDispatcher("default").forward(req, resp);
@@ -61,14 +68,16 @@ public class FrontServletController extends HttpServlet {
 
         PrintWriter out = resp.getWriter();
 
-        //out.println("Base URL: " + baseUrl);
+        // out.println("Base URL: " + baseUrl);
         url = classScanner.formatUrl(req);
         ControllerMethod match = null;
         try {
             UrlMethod urlMethod = new UrlMethod(url, req.getMethod());
             match = classScanner.validateUrlMethod(urlMethod, urlMethodMap);
-            String toPrint = urlMethod.toString() + " -> " + match.getControllerClass().getName() + " (" + match.getAssociatedMethod().getName() + ")"; 
+            String toPrint = urlMethod.toString() + " -> " + match.getControllerClass().getName() + " ("
+                    + match.getAssociatedMethod().getName() + ")";
             out.println(toPrint);
+            match.execute();
         } catch (Exception e) {
             out.println(e.getMessage() + "\n");
             out.println("Supported URLs:");
@@ -76,7 +85,8 @@ public class FrontServletController extends HttpServlet {
             // Print all supported URLs
             for (UrlMethod supportedUrl : urlMethodMap.keySet()) {
                 ControllerMethod method = urlMethodMap.get(supportedUrl);
-                String toPrint = supportedUrl.toString() + " -> " + method.getControllerClass().getName() + " (" + method.getAssociatedMethod().getName() + ")";
+                String toPrint = supportedUrl.toString() + " -> " + method.getControllerClass().getName() + " ("
+                        + method.getAssociatedMethod().getName() + ")";
                 out.println(toPrint);
             }
         }
