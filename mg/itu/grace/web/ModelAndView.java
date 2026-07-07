320a672dba8ee1aee18f2969702b055af9d2952e
MAMINIAVO Grace Tanteraka <gracetanteraka@gmail.com>
2026-07-07 07:33:38 +0300

    feat: implement ModelAndView class for view handling

---
+++ mg/itu/grace/web/ModelAndView.java
@@ -0,0 +1,33 @@
+package mg.itu.grace.web;
+
+import java.util.Map;
+import java.util.HashMap;
+
+public class ModelAndView {
+    private String viewName;
+    private Map<String, Object> attributes = new HashMap<>();
+
+    public ModelAndView(String viewName) {
+        this.viewName = viewName;
+    }
+
+    public void setViewName(String viewName) {
+        this.viewName = viewName;
+    }
+
+    public String getViewName() {
+        return viewName;
+    }
+
+    public Map<String, Object> getAttributes() {
+        return attributes;
+    }
+
+    public void setAttribute(String key, Object value) {
+        attributes.put(key, value);
+    }
+
+    public Object getAttribute(String key) {
+        return attributes.get(key);
+    }
+}
